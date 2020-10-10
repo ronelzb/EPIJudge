b988e2d88efbe0bc02850fab502b6eb59bdd5f24
ronelzb <ronelzb@gmail.com>
2020-10-10 14:03:33 -0500

    solved problem 4.2 swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    # get value of bits at positions a and b
+    a_bit = x >> i & 1
+    b_bit = x >> j & 1
+
+    # set bit at position a to b_bit and visa versa
+    x ^= (-a_bit ^ x) & (1 << j)
+    x ^= (-b_bit ^ x) & (1 << i)
+    return x
 
 
 if __name__ == '__main__':
