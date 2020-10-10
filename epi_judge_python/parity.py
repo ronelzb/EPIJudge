c3d1d7c9ec00eedb61b98d2f0286126f510cba35
ronelzb <ronelzb@gmail.com>
2020-10-10 14:03:11 -0500

    solved problem 4.1 parity of a word

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,13 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    x ^= x >> 32
+    x ^= x >> 16
+    x ^= x >> 8
+    x ^= x >> 4
+    x ^= x >> 2
+    x ^= x >> 1
+    return x & 0x1
 
 
 if __name__ == '__main__':
