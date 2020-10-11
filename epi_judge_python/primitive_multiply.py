023643135a923f817f40893e2b736a21ca79894e
ronelzb <ronelzb@gmail.com>
2020-10-11 15:13:14 -0500

    solved problem 4.5 multiply two numbers without arithmetic operations

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,29 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    is_negative = False
+
+    if x < 0 and y < 0:
+        x = -x
+        y = -y
+    elif x < 0:
+        x = -x
+        is_negative = True
+    elif y < 0:
+        y = -y
+        is_negative = True
+
+    result = 0
+
+    # multiply(x, y) = | multiply(x*2, y/2) if y is even
+    #                  | y + multiply(x*2, y/2) if y is odd
+    while y:
+        if y & 1:
+            result += x
+        x <<= 1  # multiply by 2
+        y >>= 1  # divide by 2
+
+    return -result if is_negative else result
 
 
 if __name__ == '__main__':
