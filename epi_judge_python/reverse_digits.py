be6bacb36252ad22c5ac5b72fa3653bc1e3ad7ce
ronelzb <ronelzb@gmail.com>
2020-10-20 20:53:21 -0500

    solved problem 4.8 reverse digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result, abs_x = 0, abs(x)
+
+    while abs_x:
+        result *= 10
+        result += abs_x % 10  # get most significant digit
+        abs_x //= 10  # remove least significant digit
+
+    return -result if x < 0 else result
 
 
 if __name__ == '__main__':
